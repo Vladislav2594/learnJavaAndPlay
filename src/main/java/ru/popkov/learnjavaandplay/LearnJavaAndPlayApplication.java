b512ca1897718184a747cfb2d9bd4c438d89ebb2
vladislav <vladislav.popckov.94@yandex.ru>
2024-03-18 17:49:27 +0300

    Initial commit

---
+++ src/main/java/ru/popkov/learnjavaandplay/LearnJavaAndPlayApplication.java
@@ -0,0 +1,13 @@
+package ru.popkov.learnjavaandplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaAndPlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnJavaAndPlayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/popkov/learnjavaandplay/LearnJavaAndPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.popkov.learnjavaandplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaAndPlayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
